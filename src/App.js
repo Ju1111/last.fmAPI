9045efc274125f1bbaa662815cca0872c758b973
Ju111 <SchneiderJu@gmx.net>
2018-05-30 12:23:13 +0200

    setup basic index page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './LP2.png';
 import './App.css';
+import { Welcome } from './containers/LandingPage'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <Welcome />
       </div>
     );
   }
---
+++ src/containers/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+import './LandingPage.css'
+
+export class Welcome extends PureComponent {
+
+  render() {
+    return(
+      <h2>
+        Music is the language of the soul...
+      </h2>
+    )
+  }
+}
